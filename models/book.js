b693a6bd7939fd611093def75163e50bb58c8a00
YashWadhvani <yashwadhvani123@gmail.com>
2025-05-30 21:45:27 +0530

    Model Updated

--- models/book.js
+++ models/book.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
     title: {type: String, required: true},
     author: {type: String, required: true},
-    pricce: {type: Number, required: true},
+    price: {type: Number, required: true},
     publishedDate: {type: Date, required: true}
 });
 
